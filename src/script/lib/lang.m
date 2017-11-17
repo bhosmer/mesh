f8756186f551d603c39bedb23d0ec13b14b45f79
Basil Hosmer <basil.hosmer@gmail.com>
2017-11-17 12:46:19 -0800

    comments

--- src/script/lib/lang.m
+++ src/script/lib/lang.m
@@ -36,18 +36,18 @@ intrinsic type New;         // type Nom = New(T) creates nominal type Nom over c
 intrinsic type List;        // List(T), sugar [T]
 
 // kind: (*, *) -> *
-intrinsic type Map;         // Map(K, V), sugar [K:V]
-intrinsic type Fun;         // Fun(A, B), sugar A -> B
+intrinsic type Map;         // Map(K, V), sugar [K:V]       // map:(Int, String)
+intrinsic type Fun;         // Fun(A, B), sugar A -> B      // fun:(Int, String)
 
 // kind: [*] -> *
-intrinsic type Tup;         // Tup(<type list>), sugar (T1, T2, ...)
+intrinsic type Tup;         // Tup(<type list>), sugar (T1, T2, ...)    tup:[Int, String, ...]
 
 // kind: (*, [*]) -> *
-intrinsic type Rec;         // Rec(<key type>, <type list>), sugar (k1:T1, k2:T2, ...)
-intrinsic type Var;         // Var(<key type>, <type list>), sugar (k1!T1, k2!T2, ...)
+intrinsic type Rec;         // Rec(<key type>, <type list>), sugar (k1:T1, k2:T2, ...)      rec:[x:Int, y:String, ...]
+intrinsic type Var;         // Var(<key type>, <type list>), sugar (k1!T1, k2!T2, ...)      var:[x:Int, y:String, ...]
 
 // type transformers
-intrinsic type TMap;        // type-level map: TMap(<type list>, <type constructor>)
+intrinsic type TMap;        // type-level map: TMap(<type list>, <type constructor>)    {ts,c->ts.map(c:_)}
 intrinsic type Assoc;       // experimental
 intrinsic type Cone;        // experimental
 
